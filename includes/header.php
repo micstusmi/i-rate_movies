428c790f9712ede62f544ae9cafeb3c4ce65973f
HYw <13891@ait.nsw.edu.au>
2026-03-29 22:48:30 +1100

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -24,10 +24,11 @@ if (isset($_SESSION["user_id"])) {
     <title>i-rate Movies</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="includes/style.css" rel="stylesheet">
 </head>
-<body>
+<body class="bg-gray">
 
-<nav class="navbar navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom">
   <div class="container d-flex justify-content-between">
 
     <!-- Brand -->
@@ -40,20 +41,20 @@ if (isset($_SESSION["user_id"])) {
         <!-- Main links -->
         <ul class="navbar-nav flex-row me-3">
           <li class="nav-item me-3">
-            <a class="nav-link text-white p-0" href="index.php">Home</a>
+            <a class="nav-link text-dark p-0" href="index.php">Home</a>
           </li>
           <li class="nav-item me-3">
-            <a class="nav-link text-white p-0" href="my_account.php?tab=favourites">Favourites</a>
+            <a class="nav-link text-dark p-0" href="my_account.php?tab=favourites">Favourites</a>
           </li>
           <li class="nav-item me-3">
-            <a class="nav-link text-white p-0" href="my_account.php?tab=reviews">Account</a>
+            <a class="nav-link text-dark p-0" href="my_account.php?tab=reviews">Account</a>
           </li>
         </ul>
 
         <!-- Greeting and badge -->
-        <span class="text-white me-3">
+        <span class="text-dark me-3">
           Hello,
-          <a href="my_account.php" class="text-white text-decoration-underline">
+          <a href="my_account.php" class="text-dark text-decoration-underline">
             <?php echo htmlspecialchars($_SESSION["alias"]); ?>
           </a>
           <?php if ($reviewCount >= 11): ?>
@@ -69,10 +70,10 @@ if (isset($_SESSION["user_id"])) {
         <!-- When not logged in: Home, plus Login/Register Buttons -->
         <ul class="navbar-nav flex-row me-3">
           <li class="nav-item me-3">
-            <a class="nav-link text-white p-0" href="index.php">Home</a>
+            <a class="nav-link text-dark p-0" href="index.php">Home</a>
           </li>
           <li class="nav-item me-3">
-            <a class="nav-link text-white p-0" href="about.php">About</a>
+            <a class="nav-link text-dark p-0" href="about.php">About</a>
           </li>
         </ul>
 
